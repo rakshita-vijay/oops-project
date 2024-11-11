d72d910988adc6897312dd4d0f648dede5b37a6d
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-11-11 15:06:56 +0530

    split the code into 2 files, added the logo, added code to have the blue strip on top and bottom (problems with the bottom), and the menu bar on top

---
+++ FloorPlanner.java
@@ -0,0 +1,5 @@
+public class FloorPlanner {
+  public static void main(String args[]) {
+    new Grid();
+  }
+}
--- Grid.java
+++ Grid.java
@@ -1,24 +1,97 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Grid extends JFrame{
-    Grid(){
-        this.setTitle("Grid");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
-        this.setBounds(40,40,400,400);
-        this.setVisible(true);
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(30,60));
-        panel.setBounds(150, 75, 1000, 500);
-        for(int i = 1; i <= 1800; i++){
-            JButton button = new JButton();
-            panel.add(button);
-        }
-        this.add(panel, BorderLayout.CENTER);
-    }
+public class Grid extends JFrame {
+  Grid() {
+    this.setTitle("Grid"); // title of the JFrame
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // swing does not have close by default, so cross button definition
+    this.setLayout(null); // dunno what happens, supposed to be there
+    this.setBounds(0, 20, 1400, 950); // x,y of screen; h,w of JFrame
 
-    public static void main(String args[]){
-        new Grid();
+    int rows = 40;
+    int columns = 80;
+
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(rows, columns));
+    panel.setBounds(30, 160, 1400, 700); // x,y WRT JFrame; h,w of entire JPanel (button / boxes)
+
+    for (int i = 1; i <= (rows * columns); i++) { // i <= x, where x is the number of buttons needed
+      JButton button = new JButton();
+      panel.add(button);
     }
+    this.add(panel, BorderLayout.CENTER); // BorderLayout.CENTER does not matter here
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // top strips
+
+    JLayeredPane topStrip = new JLayeredPane();
+    topStrip.setBounds(0, 0, 1500, 150);
+
+    JPanel menuPanel = new JPanel();
+    menuPanel.setBackground(Color.black);
+    menuPanel.setBounds(0, 40, 1500, 110);
+    // this.add(menuPanel);
+    topStrip.add(menuPanel);
+
+    JPanel nameStrip = new JPanel();
+    nameStrip.setBackground(Color.blue);
+    nameStrip.setBounds(0, 0, 1500, 40);
+    // this.add(nameStrip);
+    topStrip.add(nameStrip);
+
+    // logo and name
+
+    JLabel nameText = new JLabel("Heim & Leben");
+    nameText.setFont(new Font("Times New Roman", Font.BOLD, 20));
+    nameText.setForeground(Color.WHITE);
+    nameText.setBounds(50, 10, 200, 20);
+    // this.add(nameText);
+    topStrip.add(nameText, new Integer(1));
+
+    ImageIcon logo = new ImageIcon("./images/image.png");
+    JLabel logoLabel = new JLabel(logo);
+    logoLabel.setBounds(10, 10, 20, 20);
+    // this.add(logoLabel);
+    topStrip.add(logoLabel, new Integer(1));
+
+    this.add(topStrip);
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // bottom strip
+
+    // JLayeredPane bottomStrip = new JLayeredPane();
+    // bottomStrip.setBounds(0, 860, 1500, 35);
+
+    // JPanel bottomBlueStrip = new JPanel();
+    // bottomBlueStrip.setBounds(0, 860, 1500, 35);
+    // bottomBlueStrip.setBackground(Color.blue);
+    // bottomStrip.add(bottomBlueStrip, new Integer(1));
+
+    // JLabel copyrightText = new JLabel("Copyright ©VARS");
+    // copyrightText.setFont(new Font("Times New Roman", Font.BOLD, 12));
+    // copyrightText.setForeground(Color.WHITE);
+    // copyrightText.setBounds(10, 860, 200, 20);
+    // bottomStrip.add(copyrightText, new Integer(1));
+
+    // this.add(bottomStrip);
+
+    JPanel bottomBlueStrip = new JPanel();
+    bottomBlueStrip.setBounds(0, 860, 1500, 35);
+    bottomBlueStrip.setBackground(Color.blue);
+    this.add(bottomBlueStrip);
+
+    JLabel copyrightText = new JLabel("Copyright ©VARS");
+    copyrightText.setFont(new Font("Times New Roman", Font.BOLD, 12));
+    copyrightText.setForeground(Color.black);
+    copyrightText.setBounds(10, 865, 500, 20);
+    this.add(copyrightText, new Integer(1));
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // JButton menuBar = new JButton();
+
+    this.setVisible(true);
+  }
 }
