e59b205ec6c9c459cb9b5dc70fd9d7c5c019efaf
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-11-21 23:11:01 +0530

    tried to get it, the openings are not working

--- Grid.java
+++ Grid.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Grid extends JFrame {
   Grid() {
@@ -14,8 +15,8 @@ public class Grid extends JFrame {
     JLayeredPane basicGrid = new JLayeredPane();
     basicGrid.setBounds(30, 160, 1400, 700);
 
-    int rows = 40;
-    int columns = 80;
+    int rows = 35;
+    int columns = 70;
 
     JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(rows, columns));
@@ -64,6 +65,76 @@ public class Grid extends JFrame {
     // this.add(logoLabel);
     topStrip.add(logoLabel, new Integer(1));
 
+    // room, window, furniture, and fixture buttons
+
+    JButton roomLogo = new JButton();
+    roomLogo.setBounds(20, 60, 70, 70);
+
+    JLabel rl = new JLabel("R");
+    rl.setFont(new Font("Times New Roman", Font.BOLD, 50));
+    rl.setForeground(Color.black);
+    rl.setBounds(70, 10, 50, 50);
+    roomLogo.add(rl, BorderLayout.CENTER);
+
+    JLabel rn = new JLabel("ROOM");
+    rn.setFont(new Font("Times New Roman", Font.BOLD, 10));
+    rn.setForeground(Color.black);
+    rn.setBounds(5, 70, 70, 20);
+    roomLogo.add(rn, BorderLayout.CENTER);
+
+    topStrip.add(roomLogo, new Integer(2));
+
+    JButton windowLogo = new JButton();
+    windowLogo.setBounds(110, 60, 70, 70);
+
+    JLabel wl = new JLabel("W");
+    wl.setFont(new Font("Times New Roman", Font.BOLD, 50));
+    wl.setForeground(Color.black);
+    wl.setBounds(70, 10, 50, 50);
+    windowLogo.add(wl, BorderLayout.CENTER);
+
+    JLabel wn = new JLabel("WINDOW");
+    wn.setFont(new Font("Times New Roman", Font.BOLD, 10));
+    wn.setForeground(Color.black);
+    wn.setBounds(5, 70, 70, 20);
+    windowLogo.add(wn, BorderLayout.CENTER);
+
+    topStrip.add(windowLogo, new Integer(2));
+
+    JButton openingsLogo = new JButton();
+    openingsLogo.setBounds(200, 60, 70, 70);
+
+    JLabel ol = new JLabel("O");
+    ol.setFont(new Font("Times New Roman", Font.BOLD, 50));
+    ol.setForeground(Color.black);
+    ol.setBounds(70, 10, 50, 50);
+    openingsLogo.add(ol, BorderLayout.CENTER);
+
+    JLabel on = new JLabel("OPENINGS");
+    on.setFont(new Font("Times New Roman", Font.BOLD, 10));
+    on.setForeground(Color.black);
+    on.setBounds(5, 70, 70, 20);
+    openingsLogo.add(on, BorderLayout.CENTER);
+
+    topStrip.add(openingsLogo, new Integer(2));
+
+    JButton fixturesLogo = new JButton();
+    fixturesLogo.setBounds(290, 60, 70, 70);
+
+    JLabel fl = new JLabel("F");
+    fl.setFont(new Font("Times New Roman", Font.BOLD, 50));
+    fl.setForeground(Color.black);
+    fl.setBounds(70, 10, 50, 50);
+    fixturesLogo.add(fl, BorderLayout.CENTER);
+
+    JLabel fn = new JLabel("FIXTURES");
+    fn.setFont(new Font("Times New Roman", Font.BOLD, 10));
+    fn.setForeground(Color.black);
+    fn.setBounds(5, 70, 70, 20);
+    fixturesLogo.add(fn, BorderLayout.CENTER);
+
+    topStrip.add(fixturesLogo, new Integer(2));
+
     finalLayer.add(topStrip);
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -98,6 +169,20 @@ public class Grid extends JFrame {
     copyrightText.setBounds(10, 865, 500, 20);
     finalLayer.add(copyrightText, new Integer(1));
 
+    this.add(finalLayer);
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    JLayeredPane setOfRooms = new JLayeredPane();
+    setOfRooms.setBounds(30, 160, 1400, 700);
+
+    JPanel room1 = new JPanel();
+    room1.setBounds(50, 50, 100, 100);
+    room1.setBackground(Color.red);
+    room1.setBackground(new Color(100, 0, 0, 128));
+    setOfRooms.add(room1);
+
+    finalLayer.add(setOfRooms, new Integer(3));
     this.add(finalLayer);
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
